b7d9f6ecc939762f9e35ed7f26142e4b888c4c5c
muhammadrifqidwi <mrifqidwip90@gmail.com>
2025-01-15 17:49:24 +0700

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,3 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-// import AddGame from "./pages/AddGame";
-// import EditGame from "./components/EditGame";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer"; // Import Footer
-
-// const App = () => (
-//   <BrowserRouter>
-//     <div className="flex flex-col min-h-screen">
-//       {/* Navbar */}
-//       <Navbar />
-
-//       {/* Main Content */}
-//       <main className="flex-grow">
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/add-game" element={<AddGame />} />
-//           <Route path="/edit-game/:id" element={<EditGame />} />
-//         </Routes>
-//       </main>
-//       <div className="mt-10">
-//         {/* Footer */}
-//         <Footer />
-//       </div>
-//     </div>
-//   </BrowserRouter>
-// );
-
-// export default App;
-
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import AddGame from "./pages/AddGame";
